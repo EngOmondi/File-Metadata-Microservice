e2e1b149f471c18df45824c87c111a55acff0f4d
omosh1999 <83840373+omosh1999@users.noreply.github.com>
2021-09-14 03:16:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer({dest:'uploads/'})
 
 var app = express();
 
@@ -10,6 +12,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post("/api/fileanalyse",upload.single('upfile'), (req,res)=>{
+  res.json({"name":req.file.originalname,
+  "type":req.file.mimetype,
+  "size":req.file.size
+  })
+  
+})
 
 
 
